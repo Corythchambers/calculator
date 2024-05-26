529ad76c4e439397252f210672ee5e55125645d0
corythchambers <corythchambers@gmail.com>
2024-05-25 21:38:18 -0700

    Adding logic and html. installing jest

---
+++ script.js
@@ -0,0 +1,64 @@
+const add = function(num1, num2) {
+    return num1 + num2;
+}
+
+const subtract = function(num1, num2) {
+    return num1 - num2;
+}
+
+const multiply = function(num1, num2) {
+    return num1 * num2;
+}
+
+const divide = function(num1, num2) {
+    return num1 / num2;
+}
+
+let number1 = 0;
+let number2 = 0;
+let operand = '';
+
+const operate = function(n1, n2, op) {
+    switch (op) {
+        case '+':
+            return add(n1, n2);
+        case '-':
+            return subtract(n1, n2);
+        case '*':
+            return multiply(n1, n2);
+        case '/':
+            return divide(n1, n2);
+        default:
+            return "Error";
+    }
+}
+const display = document.querySelector('#display');
+const clearButton = document.querySelector('#clear');
+const opButtons = document.querySelectorAll('.operator');
+const equalsButton = document.querySelector('#equals');
+
+equalsButton.addEventListener('click', () => {
+    let valList = display.innerText.split(' ')
+    number1 = parseInt(valList[0]);
+    operand = valList[1];
+    number2 = parseInt(valList[2]);
+    display.innerText = operate(number1, number2, operand);
+})
+
+for (let opButton of opButtons ) {
+    opButton.addEventListener('click', () => {
+        display.innerHTML = display.innerHTML + " " + opButton.textContent + " ";
+    });
+}
+
+clearButton.addEventListener('click', () => {
+    display.innerText = ''
+});
+
+// create function to generate buttons? 
+const buttons = document.querySelectorAll('.number');
+for (let button of buttons) {
+    button.addEventListener('click', () => {
+        display.innerHTML += button.textContent;
+    })
+};
